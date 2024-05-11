04a3ccf5bf88b5d5c570908c48c68c40534ec9b2
yousufalansary <yousuf.alansari@gmail.com>
2024-05-11 16:55:09 +0300

    test

--- login.js
+++ login.js
@@ -1,11 +1,14 @@
-document.getElementById('loginForm').addEventListener('submit', function(event){
+document.getElementById('loginForm').addEventListener('submit', function (event) {
     event.preventDefault();
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    
+
     // Basic validation
-    if(username && password) {
+    if (username && password) {
         // Here you would normally handle the login via an API
+        if (username.toString() == "admin") {
+            window.location.href = 'admin.html'; // Redirect to the dashboard
+        }
         alert('Login successful');
         window.location.href = 'dashboard.html'; // Redirect to the dashboard
     } else {
